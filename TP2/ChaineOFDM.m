040a3289abed027ea9f8c00393ead16a4efb8fa1
jimmpanj40 <jimmy-antoine.pan@ensea.fr>
2022-10-28 13:55:57 +0200

    decision modify

--- TP2/ChaineOFDM.m
+++ TP2/ChaineOFDM.m
@@ -1,21 +1,25 @@
-function [bitsN,demapBin,demapBin0,an,xn,Xifft,Xc,Yc,Yc2,Y,Yfft,Hfft,Xfft,xn_hat,an_hat] = ChaineOFDM(SNR0,Np,h,Ncp, M)
+function [bitsN,demapBin,an,an_hat] = ChaineOFDM(SNR0,Np,h,Ncp, M)
 mComplex = const_M_QAM(M);
 mGray = Gray_M_QAM(M);
-bitsN=genBin(2*Np);
-an=mappingGray(M,bitsN,mGray,mComplex);
-xn=[0 an 0 fliplr(conj(an))];
-Xifft=ifft(xn,Np);
-Xc=[Xifft Xifft(1:Ncp)];
+
+bitsN=genBin(2*(Np-2)); %generate Np-4 bits. Np=256 so len=252
+an=mappingGray(M,bitsN,mGray,mComplex);% QAM Modulation. len=252%4=63
+xn=[0 an 0 fliplr(conj(an))];%
+Xifft=ifft(xn);
+Xc=[Xifft(end-(Ncp-1):end) Xifft];
 Yc=filter(h,1,Xc);
+Yc=Yc(1:Np+Ncp);
 Yc2=canalAWGN(Yc,(M - 1)./(3*log2(M)*SNR0));
-Y=Yc2(1:Np-Ncp);
+Y=Yc(Ncp+1:end);
 Yfft=fft(Y,Np);
 Hfft=fft(h,Np);
 Xfft=Yfft./Hfft;
-xn_hat=Xfft(2:Np/2+1);
+xn_hat=Xfft(2:Np/2);
 an_hat=decision(xn_hat,mComplex);
 demapBin0 = demapGray(an_hat,mGray,mComplex);
 demapBin = double(demapBin0 == '1');
+
+
 
 
 end
--- TP2/decision.m
+++ TP2/decision.m
@@ -1,10 +1,11 @@
 function [an2] = decision(rn,mComplex)
 %decision Summary of this function goes here
 %For each symbol of rn's sequence we find the nearest symbol of the 16-QAM
-
+an2=zeros(1,length(rn));
 closest = 0;
 for k = 1 : size(rn,2)
-    min = 5 + j*5;
+    min = abs(rn(1)-mComplex(1,1));
+    closest=mComplex(1,1);
     for i = 1 : size(mComplex,1)
         for h = 1 : size(mComplex,2)
             if min > abs(rn(1,k)-mComplex(i,h))
