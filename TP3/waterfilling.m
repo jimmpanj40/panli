79f535478940233787dd8dd3811c037c5056dddc
jimmpanj40 <jimmy-antoine.pan@ensea.fr>
2022-11-23 09:43:39 +0100

    Step 6 finished

--- TP3/waterfilling.m
+++ TP3/waterfilling.m
@@ -1,4 +1,4 @@
-function [an,am] = waterfilling(G,N0,B,Pav,N)
+function [an,am,P_allocated] = waterfilling(G,N0,B,Pav,N)
 an = G/(N0*B);
 am=an;
 M=length(am);
@@ -15,4 +15,7 @@ while condition
         condition=false; % Then, we got our ak such as the power of the system is not exceeded
     end   
 end 
+%power allocated
+P_allocated=inverseLambda-1./am
+
 end
