4dc66d22b91fb0f169cfcba57d9a0b66da6d2cc1
jimmpanj40 <jimmy-antoine.pan@ensea.fr>
2022-11-23 16:00:36 +0100

    end

--- TP3/main.m
+++ TP3/main.m
@@ -37,13 +37,13 @@ varY = var(Y)
 
 %% Step 2
 
-H = X + Y*j;
+H = X + Y*j; %Complex random signal
 varH = var(H)
 
 %% Step 3
 
 modH = abs(H);
-G = abs(H).^2;
+G = abs(H).^2; % Gain of our complex random signal
 
 % Variance
 varModH = var(modH);
@@ -74,8 +74,8 @@ X = sigma2 * randn(1,N);
 Y = sigma2 * randn(1,N);
 H = X + Y*j;
 G = abs(H).^2;
-sortedG = sort(G,'descend');
-Pav=mean(G);
+sortedG = sort(G,'descend'); % sorting the gains
+Pav=mean(G); % Power average = mean gain
 figure(3)
 plot(sortedG)
 xlabel("Rank of gains values")
@@ -89,10 +89,10 @@ N0 = 1;
 B = 1;
 N = 128;
 
-[an, am,P_allocated] = waterfilling(sortedG,N0,B,Pav,N);
+[an, am,P_allocated] = waterfilling(sortedG,N0,B,Pav,N); %waterfilling power allocation
 
 
-figure(4)
+figure(4) %entry
 subplot(211)
 axis([0 N 0 5])
 bar(an)
@@ -102,14 +102,14 @@ ylabel("Gain")
 legend("Entry")
 
 
-subplot(212)
+subplot(212)%output after waterfilling
 bar(am)
 axis([0 length(an) 0 max(an)])
 xlabel("Rank of gains value")
 ylabel("Gain")
 legend("Output")
 
-figure(5)
+figure(5) % plotting with water level explanation
 subplot(211)
 
 axis([0 length(an) 0 max(1./an)])
@@ -139,8 +139,9 @@ xlabel("Rank of gains value")
 ylabel("1/an")
 legend("Entry","Output")
 
-%Power allocated 
-figure(6)
+%Power allocated  : showing relation between the power allocated and the
+%inverse of the gain
+figure(6) 
 subplot(212)
 bar(fliplr(P_allocated))
 xlabel("Rank of the channel m")
@@ -152,7 +153,7 @@ xlabel("Rank of the channel m")
 ylabel("1/am")
 
 
-%% Step 6
+%% Step 6 
 N2=1000
 Pav=1;
 N0 = 1;
@@ -165,26 +166,106 @@ G = abs(H).^2;
 sortedG = sort(G,'descend');
 
 
-[an, am] = waterfilling(sortedG,N0,B,Pav,N2);
+[an, am, P_allocated] = waterfilling(sortedG,N0,B,Pav,N2);
 
-rate=sum(log2(1+am*Pav/N0))/length(am)
+av_rate=sum(log2(1+am.*P_allocated/N0))/length(am) %average rate
 
 %% Step 7
 N2=1000;
-
+Pav_list_dB=[1,3 6 10 13 16]; % Pav=SNR here
+Pav_list= 10.^(Pav_list_dB/10)
 N0 = 1;
 B = 1;
 
 X = N0 * randn(1,N2);
 Y = N0 * randn(1,N2);
-H = X + Y*1i;
+H = X + Y*j;
+G = abs(H).^2;
+sortedG = sort(G,'descend');
+sum_rate=zeros(1,length(Pav_list));
+
+for i=1:length(Pav_list) % calculation of the average rate for each SNR
+    [an, am, P_allocated] = waterfilling(sortedG,N0,B,Pav_list(i),N2);
+    sum_rate(i)=sum(log2(1+am.*P_allocated/N0))/length(am)/B
+end
+
+figure(7)
+plot(Pav_list_dB,sum_rate)
+xlabel("SNR in dB")
+ylabel("Sum rate in  bps/Hz")
+title("Sum rate (bps/Hz) in function of SNR (dB)")
+
+%% Step 8
+
+N_list=[16,64,128,256,512,1024,2048,4096]; 
+Pav=1;
+N0 = 1;
+B = 1;
+
+
+sum_rate=zeros(1,length(N_list));
+
+for i=1:length(N_list) % calculation of the average rate for each N
+    X = N0 * randn(1,N_list(i));
+    Y = N0 * randn(1,N_list(i));
+    H = X + Y*j;
+    G = abs(H).^2;
+    sortedG = sort(G,'descend');
+    [an, am, P_allocated] = waterfilling(sortedG,N0,B,Pav,N_list(i));
+    sum_rate(i)=sum(log2(1+am.*P_allocated/N0))/length(am)/B
+end
+
+figure(7)
+plot(N_list,sum_rate)
+xlabel("Number of samples N")
+ylabel("Sum rate in bps/Hz")
+title("Sum rate (bps/Hz) in function of N")
+
+
+%% Step 9
+
+N_opt=1024; %This value of N is good according to step 8
+Pav=1;
+N0 = 1;
+B = 1;
+
+X = N0 * randn(1,N_opt);
+Y = N0 * randn(1,N_opt);
+H = X + Y*j;
 G = abs(H).^2;
 sortedG = sort(G,'descend');
 
-SNR_list=10.^([1,3,6,10,13,16]./10);
-an_list=zeros(length(SNR_list),length(SNR_list));
-am_list=zeros(length(SNR_list),length(SNR_list));
-for i=1:length(SNR_list)
-    [an,am] = waterfilling(sortedG,N0,B,SNR_list(i),N2);
-    
+sum_rate_eq=sum(log2(1+sortedG*Pav/N0))/length(am) %the power allocated is Pav=Ptotal/N
+
+%% Step 10
+
+
+N_opt=1024;
+Pav_list_dB=[1 3 6 10 13 16]; % Pav=SNR here
+Pav_list= 10.^(Pav_list_dB/10)
+N0 = 1;
+B = 1;
+
+X = N0 * randn(1,N2);
+Y = N0 * randn(1,N2);
+H = X + Y*j;
+G = abs(H).^2;
+sortedG = sort(G,'descend');
+sum_rate_water=zeros(1,length(Pav_list));
+sum_rate_equal=zeros(1,length(Pav_list));
+
+for i=1:length(Pav_list)
+    [an, am, P_allocated] = waterfilling(sortedG,N0,B,Pav_list(i),N2);
+    sum_rate_water(i)=sum(log2(1+am.*P_allocated/N0))/length(am)/B %waterfilling sum rate
+    sum_rate_equal(i)=sum(log2(1+am.*Pav_list(i)/N0))/length(am)/B %equal power allocation sum rate
 end
+
+figure(8)
+plot(Pav_list_dB,sum_rate_water)
+hold on 
+plot(Pav_list_dB,sum_rate_equal)
+
+xlabel("SNR in dB")
+ylabel("Sum rate in  bps/Hz")
+title("Sum rate (bps/Hz) in function of SNR (dB)")
+legend('waterfilling algorithm power allocation','equal power allocation')
--- TP3/waterfilling.m
+++ TP3/waterfilling.m
@@ -4,18 +4,18 @@ am=an;
 M=length(am);
 
 condition= true ; %Condition which indicate if we continue or stop the waterfilling algorithm
-P=N*Pav;
+P=N*Pav; %Total power
 
 while condition
-    inverseLambda=(P+sum(1./am))/M;
-    if isempty(find((inverseLambda-1./am)<0)) == false
-        am=am(1:end-1);
-        M=length(am);
+    inverseLambda=(P+sum(1./am))/M; 
+    if isempty(find((inverseLambda-1./am)<0)) == false %check if inverseLambda-1/ak are all positive
+        am=am(1:end-1); % if not, we remove last am
+        M=length(am); %we adapt M, the length of am
     else 
         condition=false; % Then, we got our ak such as the power of the system is not exceeded
     end   
 end 
-%power allocated
-P_allocated=inverseLambda-1./am
+P_allocated=inverseLambda-1./am; %power allocated to each channels of gain ak
+
 
 end
